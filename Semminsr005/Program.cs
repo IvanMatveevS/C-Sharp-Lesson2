bbc31803e890d7533555a521b19c2abd63dfa4b0
Ivan <ivanmatveev1001@gmail.com>
2022-08-09 21:38:05 +0300

    второй вариант два числа одно из них квадрат другого

--- Semminsr005/Program.cs
+++ Semminsr005/Program.cs
@@ -1,6 +1,6 @@
 ﻿int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
-if( a / b == b ^ b / a == a)
+if( a * a == b ^ b * b == a)
 {
     Console.WriteLine("да");
     
