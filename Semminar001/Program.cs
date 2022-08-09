7633b374f8c25b9825c5b530c66c3ac127e7df41
Ivan <ivanmatveev1001@gmail.com>
2022-08-09 20:24:12 +0300

    Задание 1 число на рандом и цифра наибольшая

---
+++ Semminar001/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = new Random().Next(10, 100); //рандомное число из интервала от 10 до 100
+
+Console.WriteLine(a);
+
+int FirstDigit = a / 10;
+int SecondDigit = a % 10;
+
+if (FirstDigit > SecondDigit)
+{
+    Console.WriteLine("Наибольшая цифра" + FirstDigit);
+}
+else
+{
+     Console.WriteLine("Наибольшая цифра" + SecondDigit);
+}
