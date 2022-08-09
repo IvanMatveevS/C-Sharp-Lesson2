9c6417fb370007b4e0711bc776b860967ef30c84
Ivan <ivanmatveev1001@gmail.com>
2022-08-09 20:53:41 +0300

    кратно или остаток от деления двух чисел

---
+++ Semminar003/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if( a % b == 0)
+{
+    Console.WriteLine("Кратно");
+    
+}
+else 
+{
+    Console.WriteLine( "Остаток " + a % b );
+}
