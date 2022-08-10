6b268f7ffddcccf4ef56b56c62fb42caa5ea4ce2
Ivan <ivanmatveev1001@gmail.com>
2022-08-10 09:40:08 +0300

    Задание 3

---
+++ HW3/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Введите целое число от 1 до 7");
+int n = Convert.ToInt32(Console.ReadLine());
+switch (n)
+{
+case 1:
+{
+    Console.WriteLine("Нет");
+    break;
+}
+case 2:
+{
+    Console.WriteLine("Нет");
+    break;
+}
+
+case 3:
+{
+    Console.WriteLine("Нет");
+   break;
+}
+case 4:
+{
+    Console.WriteLine("Нет");
+    break;
+}
+case 5:
+{
+    Console.WriteLine("Нет");
+    break;
+}
+case 6:
+{
+    Console.WriteLine("Да");
+    break;
+}
+case 7:
+{
+    Console.WriteLine("Да");
+    break;
+}
+
+}
