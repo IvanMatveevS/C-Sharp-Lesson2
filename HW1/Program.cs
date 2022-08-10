1c57ca2fec0a03452ce76480b111671b2306da44
Ivan <ivanmatveev1001@gmail.com>
2022-08-10 09:55:28 +0300

    второе число

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,6 +1,7 @@
-﻿int a = new Random().Next(100, 1000);
-Console.WriteLine(a);
+﻿Console.WriteLine("Введите число от 100 до 999: ");
+int a = Convert.ToInt32(Console.ReadLine());
 
+Console.WriteLine("Второе число: ");
 int SecondDigit = a / 10;
 int chek = SecondDigit % 10;
 Console.WriteLine(chek);
