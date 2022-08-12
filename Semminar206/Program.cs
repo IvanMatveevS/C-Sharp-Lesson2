faec3f8bbe83438d68e53c589ebde99ea83de98c
Ivan <ivanmatveev1001@gmail.com>
2022-08-12 21:21:43 +0300

    коммит всего

---
+++ Semminar206/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите целst 2 число ");
+int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+if( x > 0)
+{ 
+    if (y > 0)
+    {
+        Console.WriteLine("1ая ");
+    }
+    else
+    {
+        Console.WriteLine("4ая ");
+    }
+}
+else
+{
+    if (y > 0)
+    {
+        Console.WriteLine("2ая ");
+    }
+    else
+    {
+        Console.WriteLine("3ая ");
+    }
+
+}
