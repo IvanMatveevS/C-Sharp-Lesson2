6886d0cf5685d2b997f784e9e14c570fefef3ed6
Ivan <ivanmatveev1001@gmail.com>
2022-08-14 07:00:56 +0300

    эксперимент с гит

---
+++ Semminar208/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите номер четверти (от 1 до 4):");
+int n = Convert.ToInt32(Console.ReadLine());
+switch (n)
+{
+case 1:
+{
+    Console.WriteLine("x > 0, y > 0");
+    break;
+}
+case 2:
+{
+    Console.WriteLine(" x < 0, y > 0 ");
+    break;
+}
+
+case 3:
+{
+    Console.WriteLine(" x < 0, y < 0");
+   break;
+}
+case 4:
+{
+    Console.WriteLine(" x > 0, y < 0");
+    break;
+}
+}
