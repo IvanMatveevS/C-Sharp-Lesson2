c483f9cb1cb9fffed6e86ebd284218adc68fd8ff
Ivan <ivanmatveev1001@gmail.com>
2022-08-14 07:54:19 +0300

    2D координаты

---
+++ Semminar209/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите координаты первой точки");
+int ax = int.Parse(Console.ReadLine());
+int ay = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки");
+int bx = int.Parse(Console.ReadLine());
+int by = int.Parse(Console.ReadLine());
+
+double AB = Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2));
+Console.WriteLine(AB);
