ae3dea3de34aea89842fe9bb5b85041a15e4a4cf
Ivan <ivanmatveev1001@gmail.com>
2022-08-14 07:44:21 +0300

    задание с 3D координаты

---
+++ HomWor1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите координаты первой точки");
+int ax = int.Parse(Console.ReadLine());
+int ay = int.Parse(Console.ReadLine());
+int az = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки");
+int bx = int.Parse(Console.ReadLine());
+int by = int.Parse(Console.ReadLine());
+int bz = int.Parse(Console.ReadLine());
+
+double AB = Math.Sqrt(Math.Pow(ax - bx, 2) + Math.Pow(ay - by, 2) + Math.Pow(az - bz, 2));
+Console.WriteLine(AB);
